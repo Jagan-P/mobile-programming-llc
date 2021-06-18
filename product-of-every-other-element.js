816c33c46a9a0f19e4cf55986d26f053194fe6f6
Jagan.P <jagan.p@plantix.net>
2021-06-18 10:28:11 +0530

    O(n*n) to O(2n) for product-of-every-other-element

--- product-of-every-other-element.js
+++ product-of-every-other-element.js
@@ -1,13 +1,20 @@
 // 1. Given an input array = [1, 2, 3, 4]. return the following output [24, 12, 8, 6]. i.e. the output array element is the product of every other element.
 
 let array = [1, 2, 3, 4];
+let product = array.reduce((a, b)=>{
+    return a*b;
+})
 
-for(let i=0;i<array.length; i++) {
-    let product = 1;
-    for(let j=0; j<array.length; j++) {
-        if(i!==j) {
-            product=product*array[j];
-        }
-    }
-    console.log(product);
+for (let i=0; i<array.length; i++) {
+    array[i] = product/array[i];
 }
+console.log(array);
+// for(let i=0;i<array.length; i++) {
+//     let product = 1;
+//     for(let j=0; j<array.length; j++) {
+//         if(i!==j) {
+//             product=product*array[j];
+//         }
+//     }
+//     console.log(product);
+// }
